6cc43f3ceb1b68ac2012028f4bac7733cb106735
ThomasAlberto21 <thomasalberto456@gmail.com>
2023-02-21 20:37:43 +0700

    Membuat View

---
+++ View/TodolistView.php
@@ -0,0 +1,33 @@
+<?php
+
+
+namespace View {
+
+
+    class TodolistView
+    {
+
+        /**
+         * This function show a todo list
+         */
+        function showTodolist(): void
+        {
+        }
+
+
+        /**
+         * This function adds a todo list.
+         */
+        function addTodolist(): void
+        {
+        }
+
+
+        /**
+         * It removes the todolist.
+         */
+        function removeTodolist(): void
+        {
+        }
+    }
+}
