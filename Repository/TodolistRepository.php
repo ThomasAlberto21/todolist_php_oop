dfdc8634439d1ccf76e6c123990b96ee11223b80
ThomasAlberto21 <thomasalberto456@gmail.com>
2023-02-21 21:01:20 +0700

    Repositiry dan Service: Menampilkan Todolist

--- Repository/TodolistRepository.php
+++ Repository/TodolistRepository.php
@@ -3,11 +3,10 @@
 
 namespace Repository {
 
-    /* Importing the TodoList class from the Entitiy namespace. */
+
     use Entitiy\TodoList;
 
 
-    /* Creating an interface called TodolistRepository. */
     interface TodolistRepository
     {
 
@@ -21,5 +20,49 @@ namespace Repository {
 
         /* A function that takes no parameters and returns an array. */
         function findAll(): array;
+    }
+
+
+
+    class TodolistRepositoryImpl implements TodolistRepository
+    {
+
+        /* Declaring a private variable called `` and initializing it to an empty array. */
+        private array $todolist = array();
+
+
+
+        /**
+         * `save` takes a `TodoList` object and saves it to the database
+         * 
+         * @param TodoList todoList The TodoList object to be saved.
+         */
+        function save(TodoList $todoList): void
+        {
+        }
+
+
+
+        /**
+         * `remove` takes an integer and returns a boolean
+         * 
+         * @param int number The number to remove from the list.
+         */
+        function remove(int $number): bool
+        {
+        }
+
+
+
+
+        /**
+         * This function returns an array of all the todo items
+         * 
+         * @return array An array of todolist
+         */
+        function findAll(): array
+        {
+            return $this->todolist;
+        }
     }
 }
--- Service/TodolistService.php
+++ Service/TodolistService.php
@@ -3,8 +3,8 @@
 
 namespace Service {
 
+    use Repository\TodolistRepository;
 
-    /* Creating a new interface called TodolistService. */
     interface TodolistService
     {
         /* A function that shows the todolist. */
@@ -15,5 +15,63 @@ namespace Service {
 
         /* Removing the todolist. */
         function removeTodolist(int $number): void;
+    }
+
+
+
+    class TodolistServiceImpl implements TodolistService
+    {
+
+        private TodolistRepository $todolistRepository;
+
+
+        /**
+         * The constructor function is a special function that is called when an object is created from
+         * a class
+         * 
+         * @param TodolistRepository todolistRepository This is the name of the class that we want to
+         * inject.
+         */
+        public function __construct(TodolistRepository $todolistRepository)
+        {
+            $this->todolistRepository = $todolistRepository;
+        }
+
+
+
+
+        /**
+         * It shows the todolist.
+         */
+        function showTodolist(): void
+        {
+            echo "TODO LIST" . PHP_EOL;
+            $todolist = $this->todolistRepository->findAll();
+            foreach ($todolist as $number => $value) {
+                echo "$number. $value" . PHP_EOL;
+            }
+        }
+
+
+
+        /**
+         * `addTodolist` adds a todo list
+         * 
+         * @param string todo The todo item to add to the list.
+         */
+        function addTodolist(string $todo): void
+        {
+        }
+
+
+
+        /**
+         * This function removes a todo list from the database
+         * 
+         * @param int number The number of the todolist to remove.
+         */
+        function removeTodolist(int $number): void
+        {
+        }
     }
 }
